818727df9d42c8b22195803a182966835bb004c2
Aslan <keepaoob@gmail.com>
2021-01-26 23:43:38 +0600

    Added steam sessions, entry level, core classes and basic UI

---
+++ Source/MashedPotatoes/Public/BaseGameInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "BaseGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASHEDPOTATOES_API UBaseGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE bool HasNickname() const { return Nickname.IsSet(); }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE FString GetNickname() const { return Nickname.Get(TEXT("No name")); }
+
+	UFUNCTION(BlueprintCallable)
+	void SetNickname(const FString& NewNickname) { Nickname = NewNickname; }
+	
+	UPROPERTY(BlueprintReadWrite)
+	FString PlayerNickname = TEXT("No name");
+
+protected:
+	TOptional<FString> Nickname;
+};
